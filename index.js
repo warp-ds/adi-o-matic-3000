145ca6c2941004775374a682b48f4df8cf15d97d
Niklas Wikstrand <niklas.wikstrand.nordahl@schibsted.com>
2023-02-18 09:44:57 +0100

    fix: handle objects and values that collides

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
 import fs from 'node:fs'
 import yaml from 'yaml'
-import lodash from 'lodash'
 
 const csv = fs.readFileSync('./in.csv', 'utf-8')
 const lines = csv.split('\n')
 const collected = {}
+
 for (const line of lines) {
-  const [name, target] = line.split(',')
+  const [name, value] = line.split(',')
   const segments = name.split('-')
-  lodash.set(collected, segments.join('.'), target)
+
+  segments.reduce((acc, currentValue, index) => {
+    const lastItem = index + 1 === segments.length;
+
+    // set value as string or object containg the value and previous values depending on last item type
+    if(lastItem) {
+      if(typeof acc[currentValue] === 'object') {
+        return acc[currentValue] = { ...acc[currentValue], "_": value }
+      }
+
+      return acc[currentValue] = value
+    }
+
+    // rewrite currentValue as an object if its a string
+    if(typeof acc[currentValue] === 'string') {
+      return acc[currentValue] = { "_": acc[currentValue] }
+    }
+
+    return acc[currentValue] = acc[currentValue] || {};
+  }, collected);
 }
+
 const out = yaml.stringify(collected)
 console.log(out)
