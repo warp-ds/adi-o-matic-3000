41fd43a55581c9547ba68069c20f2cdc655af1d8
Niklas Wikstrand <niklas.wikstrand.nordahl@schibsted.com>
2023-02-20 13:25:10 +0100

    chore: add vitest and super basic snapshot test for adi-o-matic 3000 script

---
+++ adi-o-matic-3000.js
@@ -0,0 +1,35 @@
+import yaml from 'yaml'
+const collected = {}
+
+function adiOMatic3000(csv) {
+
+  const lines = csv.split('\n')
+  for (const line of lines) {
+    const [name, value] = line.split(',')
+    const segments = name.split('-')
+
+    segments.reduce((acc, currentValue, index) => {
+      const lastItem = index + 1 === segments.length;
+
+      // set value as string or object containg the value and previous values depending on last item type
+      if(lastItem) {
+        if(typeof acc[currentValue] === 'object') {
+          return acc[currentValue] = { ...acc[currentValue], "_": value }
+        }
+
+        return acc[currentValue] = value
+      }
+
+      // rewrite currentValue as an object if its a string
+      if(typeof acc[currentValue] === 'string') {
+        return acc[currentValue] = { "_": acc[currentValue] }
+      }
+
+      return acc[currentValue] = acc[currentValue] || {};
+    }, collected);
+  }
+
+  return yaml.stringify(collected)
+}
+
+export { adiOMatic3000 }
--- index.js
+++ index.js
@@ -1,34 +1,8 @@
 import fs from 'node:fs'
-import yaml from 'yaml'
+import { adiOMatic3000 } from './adi-o-matic-3000.js'
 
-const csv = fs.readFileSync('./in.csv', 'utf-8')
-const lines = csv.split('\n')
-const collected = {}
+const csv = fs.readFileSync('./mock-data.csv', 'utf-8')
 
-for (const line of lines) {
-  const [name, value] = line.split(',')
-  const segments = name.split('-')
+const result = adiOMatic3000(csv);
 
-  segments.reduce((acc, currentValue, index) => {
-    const lastItem = index + 1 === segments.length;
-
-    // set value as string or object containg the value and previous values depending on last item type
-    if(lastItem) {
-      if(typeof acc[currentValue] === 'object') {
-        return acc[currentValue] = { ...acc[currentValue], "_": value }
-      }
-
-      return acc[currentValue] = value
-    }
-
-    // rewrite currentValue as an object if its a string
-    if(typeof acc[currentValue] === 'string') {
-      return acc[currentValue] = { "_": acc[currentValue] }
-    }
-
-    return acc[currentValue] = acc[currentValue] || {};
-  }, collected);
-}
-
-const out = yaml.stringify(collected)
-console.log(out)
+console.log(result)
